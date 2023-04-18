75e88ce5ae106b43922062350f674db439dd17bd
DLEIVA <dleivaur@gmail.com>
2023-04-18 17:59:54 +0200

    Changes in SimData to have 30 vars

--- data/CleaningData.R
+++ data/CleaningData.R
@@ -6,5 +6,5 @@ mydat <- read_sav('datos.sav')
 
 # Try simulating some data
 set.seed(321)
-mydat2 <- data.frame(matrix(rnorm(1000),100,10))
-colnames(mydat2) <- paste0('V',1:10)
+mydat2 <- data.frame(matrix(rnorm(3000),100,30))
+colnames(mydat2) <- paste0('V',1:30)
