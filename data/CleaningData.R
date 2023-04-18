5faecc0bc11e78e9e0f37c372099c40289072776
DLEIVAUR79 <dleivaur@ub.edu>
2023-04-18 17:58:38 +0200

    Sim data to have 20 vars

--- data/CleaningData.R
+++ data/CleaningData.R
@@ -6,5 +6,5 @@ mydat <- read_sav('datos.sav')
 
 # Try simulating some data
 set.seed(321)
-mydat2 <- data.frame(matrix(rnorm(1000),100,10))
-colnames(mydat2) <- paste0('V',1:10)
+mydat2 <- data.frame(matrix(rnorm(2000),100,20))
+colnames(mydat2) <- paste0('V',1:20)
